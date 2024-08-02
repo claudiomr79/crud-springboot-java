4da955dd4051d1227e4e1c10da7324ae5ae23fe5
Claudio Rohr <claudiomr79@gmail.com>
2024-08-02 12:35:00 -0300

    primer commit del crud springboot

---
+++ src/main/java/com/api/crud/services/UserService.java
@@ -0,0 +1,16 @@
+package com.api.crud.services;
+
+import com.api.crud.models.UserModel;
+import com.api.crud.repositories.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+
+@Service
+public class UserService {
+    @Autowired
+    IUserRepository userRepository;
+    public ArrayList<UserModel> getUsers() {
+        return (ArrayList<UserModel>) userRepository.findAll();
+    }
+}
